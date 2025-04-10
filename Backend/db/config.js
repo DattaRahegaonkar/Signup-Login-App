26460f0b7a49372bf8081ad7f86ada6d0fb02140
Datta Rahegaonkar <rahegaonkardatta@gmail.com>
2025-04-11 01:40:55 +0530

    Added Changes

---
+++ Backend/db/config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/E-Commerce')
+  .then(() => console.log('Connected!'));
+
+const productschema = new mongoose.Schema({});
+
+const Products = mongoose.model("Products", productschema)
