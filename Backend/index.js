49c47b36f6da37a05bca31d03990963adf4a077a
Datta Rahegaonkar <rahegaonkardatta@gmail.com>
2025-04-26 22:35:46 +0530

    Added

--- Backend/index.js
+++ Backend/index.js
@@ -99,6 +99,8 @@ app.patch("/update", (req, res) => {
 //   res.send("ok");
 // });
 
-app.listen(3000, () => {
+const port = process.env.PORT | 5000
+
+app.listen(port, () => {
   console.log("Server is Running");
 });
